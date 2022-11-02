9afd3a0d970cf937ffa2e149ac66ccfc1a8ceb21
Vihresko <vihrenstefanov@gmail.com>
2022-11-02 14:33:51 +0200

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+//DI , Serilog, Settings
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT") ?? "Production"}.json", optional:true)
+        .AddEnvironmentVariables();
+}
+
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Logger.Information("Application Starting");
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddTransient<IGreetingService, GreetingService>();
+    })
+    .UseSerilog()
+    .Build();
+
+var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+svc.Run();
